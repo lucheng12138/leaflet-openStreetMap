ff79ed78f07835fb3988368d43e206e1d53e714e
lucheng12138 <625123748@qq.com>
2017-06-16 13:37:43 +0800

    Update map.js

--- testmap/map.js
+++ testmap/map.js
@@ -11,14 +11,14 @@ L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 function getColor(c) {
-    return c > 80 ? "#800026" :
-        c > 70 ? "#BD0026" :
-            c > 60 ? "#333" :
+    return c > 80 ? "#3194D0" :
+        c > 70 ? "#fbd0e5" :
+            c > 60 ? "#c4bcda" :
                 c > 50 ? "#E31A1C" :
                     c > 40 ? "#FC4E2A" :
-                        c > 30 ? "#FD8D3C" :
+                        c > 30 ? "#f7d2b6" :
                             c > 20 ? "#FEB24C" :
-                                c > 10 ? "#FED976" :
+                                c > 10 ? "#c2e4b7" :
                                     "#FFEDA0";
 }
 
@@ -33,12 +33,10 @@ function style(feature) {
     };
 }
 
-L.geoJson(chinaPro, { style: style }).addTo(map);
-
 function markerFeature(e) {
     var layer = e.target;
     layer.setStyle({
-        weight: 5,
+        weight: 3,
         color: "#333",
         dashArray: "",
         fillOpacity: 0.7
@@ -57,17 +55,10 @@ function resetmarker(e) {
     geojson.resetStyle(e.target);
 }
 
-function popupInfo(e) {
-    lay = [e.target.feature.properties.cp[1], e.target.feature.properties.cp[0]];
-    var geo = e.target.feature.properties;
-    L.popup().setLatLng(lay).setContent('<p>id:'+geo.id+'<br>name:'+geo.name+'<br>childNum:'+geo.childNum+'<br>cp:['+geo.cp[1]+','+geo.cp[0]+']</p>').openOn(map);
-};
-
 function onEachFeature(feature, layer) {
     layer.on({
         mouseover: markerFeature,
-        mouseout: resetmarker,
-        click: popupInfo
+        mouseout: resetmarker
     });
 }
 
@@ -75,6 +66,9 @@ geojson = L.geoJson(chinaPro, {
     style: style,
     onEachFeature: onEachFeature
 }).addTo(map);
+geojson.eachLayer(function (layer) {
+    layer.bindPopup("childNum:"+layer.feature.properties.childNum+"<br>id:"+layer.feature.properties.id+"<br>name:"+layer.feature.properties.name+"<br>cp:["+layer.feature.properties.cp[0]+","+layer.feature.properties.cp[1]+"]");
+});
 
-var bounds = [[-52, 40],[75,270]];
-var image = L.imageOverlay("640.png",bounds).addTo(map);
+var bounds = [[-52, 40], [75, 270]];
+var image = L.imageOverlay("640.png", bounds).addTo(map);
